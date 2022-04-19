3c7f42b49ad922e0937915022e52e2256cf2bb29
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-04-18 20:09:12 -0500

    TEST 3 FORMS

---
+++ correos.php
@@ -0,0 +1,15 @@
+<?php
+$destinatario = 'info@teduemprende.com'
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$ciudad = $_POST['ciudad'];
+
+
+$header = "Enviado desde teduemprende.com";
+$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+mail($destinatario, $nombre, $mensajeCompleto, $header);
+echo "<script>setTimeou(\"location.href='index.html'\", 1000)</script>"
+?>
