a74093bf704c3283de1f1be35790d0f1a7d0fcff
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-04-25 11:21:30 -0500

    FORM IS WORKING FINALLY

--- enviar.php
+++ enviar.php
@@ -1,84 +1,45 @@
 
 <?php
-
+header("Content-Type: text/html; charset=UTF-8", true);
 error_reporting(1);
 
-function correo($correos,$nombre){
- require_once("phpmailer/class.phpmailer.php");
+function correo($correos,$nombre,$carta){
+    require_once("phpmailer/class.phpmailer.php");
+   
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
 
-  $mail = new phpmailer();
+    $mail->PluginDir = "phpmailer/";
+    //$mail->IsSMTP();
+    $mail->SMTPDebug = 2;
 
-  $mail->PluginDir = "phpmailer/";
-  $mail->CharSet = 'UTF-8';
-  //$mail->isSMTP();
-
-  //Con la propiedad Mailer le indicamos que vamos a usar un
-  //servidor smtp
-  $mail->Mailer = "smtp";
-  $mail->SMTPDebug  = 0;
-  $mail->Debugoutput = 'html';
-
-  //Asignamos a Host el nombre de nuestro servidor smtp
-  $mail->Host ="smtp.gmail.com";
-  $mail->Port = 587;
-  //Le indicamos que el servidor smtp requiere autenticación
-  $mail->SMTPAuth = true;
-  $mail->SMTPSecure = 'tls';
-  $mail->SMTPOptions = array(
-            'ssl' => array('verify_peer' => false,'verify_peer_name' => false,'allow_self_signed' => true)
-        );
-  //Le decimos cual es nuestro nombre de usuario y password
-  $mail->Username ="teduemprende@gmail.com";//debes de poner un correo de gmail que tenga configurado para aplicaciones no seguras en la parte de gmail
-  $mail->Password ="EmprendeconTedu2022"; // debes de poner la clave del correo
-
-  //Indicamos cual es nuestra dirección de correo y el nombre que
-  //queremos que vea el usuario que lee nuestro correo
-  $mail->From = "info@teduemprende.com";
-  $mail->FromName = "TEDU EMPRENDE";
-
-  //el valor por defecto 10 de Timeout es un poco escaso dado que voy a usar
-  //una cuenta gratuita, por tanto lo pongo a 30
-  $mail->Timeout=30;
-
-  //Indicamos cual es la dirección de destino del correo
-  $mail->AddAddress($correos,$nombre);
-
-
-  //Asignamos asunto y cuerpo del mensaje
-  //El cuerpo del mensaje lo ponemos en formato html, haciendo
-  //que se vea en negrita
-
-  $mail->Subject = "Reserva para asistir al evento del lanzamiento digital de TEDU EMPRENDE";
-  $fecha_actual = date("d-m-Y");
-  $shtml="Estimado ".$nombre."<br><br>TEDU agradece su registro de asistencia, estaremos gustosos en recibirte en nuestro lanzamiento.<br><br>Tendremos grandes sorpresas.";
-  $mail->msgHTML($shtml);
-
-  $mail->IsHTML(true);
-
-  $exito = $mail->Send();
-
-  //Si el mensaje no ha podido ser enviado se realizaran 4 intentos mas como mucho
-  //para intentar enviar el mensaje, cada intento se hara 5 segundos despues
-  //del anterior, para ello se usa la funcion sleep
-  $intentos=1;
-  while ((!$exito) && ($intentos < 5)) {
-	sleep(5);
-     	//echo $mail->ErrorInfo;
-     	$exito = $mail->Send();
-     	$intentos=$intentos+1;
-   }
-
-
-   if(!$exito)
-   {
-		echo "Problemas enviando el correo electronico a ".$correos;
-		echo "<br/>".$mail->ErrorInfo;
-   }
-   else
-   {
-	    echo "Enviado con exito";
-   }
+    $mail->Host = 'mail.teduemprende.com';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'avisos@teduemprende.com';
+    $mail->Password = 'EmprendeconTedu2022';
+    $mail->setFrom('avisos@teduemprende.com', 'Equipo TEDU');
+    
+    //$mail->addReplyTo('info@teduemprende.com', 'Equipo TEDU');
+    $mail->addAddress($correos, $nombre);
+    $mail->addAddress('teduemprende@gmail.com', 'Equipo TEDU');
+    
+    $mail->Subject ="Lanzamiento de TEDU, la fabrica de negocios **MUY PRONTO**";
+    
+    $contenido = "Estimad@ ".$nombre."\n\nAgradecemos que hayas sido participe en nuestro proximo lanzamiento a nuestra gran apertura que te cambiara la vida.\n\nTus datos de contactos son:\n".$carta."\n\nTendremos grandes sorpresas.\n\nEquipo TEDU\n\n\nPor favor no responder este correo";
+    
+    //$mail->msgHTML(file_get_contents('contenido.html'), __DIR__);
+    $mail->Body = $contenido;
+    //$mail->IsHTML(true);
+    //$mail->addAttachment('test.txt');
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'The email message was sent.';
+    }
 }
+
 
 	//LLAMANDO A LOS CAMPOS
     $nombre=$_POST['nombre'];
@@ -90,13 +51,13 @@ function correo($correos,$nombre){
     $destinatario = "info@teduemprende.com";
     $asunto = "Contacto desde la web";
 
-    $carta = "De: $nombre \n";
-    $carta .= "Correo: $correo \n";
-    $carta .= "Teléfono: $telefono \n";
-    $carta .= "Ciudad: $ciudad";
+    $carta = "\n"."Participante:".$nombre."\n";
+    $carta .= "Correo:".$correo."\n";
+    $carta .= "Telefono:".$telefono."\n";
+    $carta .= "Ciudad:".$ciudad."\n\n";
 
     //ENVIANDO MENSAJE
     //mail($destinatario, $asunto, $carta);
-	correo($correo,$nombre);
+	correo($correo,$nombre,$carta);
 	header('Location:index.html')
 ?>
