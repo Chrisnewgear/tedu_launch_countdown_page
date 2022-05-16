2cf236fb4e5ae2a182ea1fd4abfa12c25f2afbfe
Chrisnewgear <chrisnewgear2011@gmail.com>
2022-05-16 11:49:30 -0500

    MAIL ADDED

--- JS/countdown.js
+++ JS/countdown.js
@@ -2,9 +2,9 @@
     simplyCountdown('#countdown', {
         year: 2022, // required
         month: 5, // required
-        day: 17, // required
-        hours: 0, // Default is 0 [0-23] integer
-        minutes: 0, // Default is 0 [0-59] integer
+        day: 25, // required
+        hours: 6, // Default is 0 [0-23] integer
+        minutes: 20, // Default is 0 [0-59] integer
         seconds: 0, // Default is 0 [0-59] integer
         words: { //words displayed into the countdown
             days: { singular: 'Día', plural: 'Días' },
